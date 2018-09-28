3f02162c041a9df5543bac119377f1c1910c4844
Julio Vinachi <julio899@gmail.com>
2018-09-28 14:14:35 -0400

    agregando un paginador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import axios from 'axios';
 
 let info=null;
-
+let paginator={};
 
 @Component({
   selector: 'app-root',
@@ -12,24 +12,41 @@ let info=null;
 export class AppComponent {
   title = 'Test Skills in Angular 6';
   clientes=null;
-  cantidad=0;
+  cantidad:number=0;
   selectClient:null;
-
+  indice_start:number=0;
+  count_pages:number=0;
+  users_per_pages:number=5;
+  pages=[];
   constructor(){
   	// Make a request for a user with a given ID
-	axios.get('https://randomuser.me/api/?results=10')
+	axios.get('https://randomuser.me/api/?results=50')
 	  .then((response)=>{
 	    // handle success
 	    console.log(response.data.results);
 	    info=response.data.results;
-	    this.clientes=info;
 	    this.cantidad=info.length;
-	    console.log(this.clientes);
+	    this.count_pages=this.cantidad/this.users_per_pages;
 	     //i Like to Preload All images the profile
 	     for (var i = info.length - 1; i >= 0; i--) {
 	     	var imageObject = new Image();
 	        imageObject.src = info[i].picture.large;
+	        info[i].key=i;
 	     }
+
+	     //order pages
+	     for (var o =0; o < this.count_pages; o++) {
+	     	var inicio=o*(this.users_per_pages-1);
+	     	var final=inicio+this.users_per_pages;
+	     	paginator[o]={page:o+1,start:inicio,end:final};	    		
+	     	//this.pages[o]=new Array({page:o+1,start:inicio,end:final});
+	     	this.pages[o]={page:o+1,start:inicio,end:final}	;
+	     }
+	     console.log(paginator)
+	     console.log(this.pages)
+
+	    this.clientes=info;
+	    console.log(this.clientes);
 	  })
 	  .catch(function (error) {
 	    // handle error
@@ -49,6 +66,11 @@ onSelect(obj): void {
 closedModal(): void {
   this.selectClient = null;
   document.getElementById("modal").style.display="none";
+}
+
+changePage(obj):void{
+	console.log(obj);
+	this.indice_start=obj.start;
 }
 
 
